67f3ce4a3b0fe621b501f0e35d6e6db942756c5f
efecarranza <fcarr004@fiu.edu>
2023-01-19 16:02:14 -0500

    feat: create v3 deployer

--- script/DeployContract.s.sol
+++ script/DeployContract.s.sol
@@ -3,14 +3,27 @@ pragma solidity ^0.8.15;
 
 import "@forge-std/console.sol";
 import {Script} from "@forge-std/Script.sol";
-import {Contract} from "../src/Contract.sol";
+import {DefaultReserveInterestRateStrategy} from "@aave-v3/protocol/pool/DefaultReserveInterestRateStrategy.sol";
+import {IPoolAddressesProvider} from "@aave-v3/interfaces/IPoolAddressesProvider.sol";
 
 contract DeployContract is Script {
-    uint256 public constant NUM = 7;
+    IPoolAddressesProvider internal constant POOL_ADDRESSES_PROVIDER =
+        IPoolAddressesProvider(0xa97684ead0e402dC232d5A977953DF7ECBaB3CDb);
 
     function run() external {
         vm.startBroadcast();
-        Contract deployedContract = new Contract(NUM);
+        DefaultReserveInterestRateStrategy deployedContract = new DefaultReserveInterestRateStrategy(
+            POOL_ADDRESSES_PROVIDER,
+            800000000000000000000000000, // UOptimal
+            30000000000000000000000000, // Base Variable Borrow Rate
+            140000000000000000000000000, // Variable Slope 1
+            1500000000000000000000000000, // Variable Slope 2
+            0, // Stable Slope 1
+            0, // Stable Slope 2
+            20000000000000000000000000, // Base Stable Rate Offset
+            50000000000000000000000000, // Stable Rate Excess Offset
+            200000000000000000000000000 // Optimal Stable To Total Debt Ratio
+        );
         console.log("Contract address: ", address(deployedContract));
         vm.stopBroadcast();
     }
